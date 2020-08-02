292445521da53654f1e8185e5978fb66da322470
thanamat <thanamat@odds.team>
2020-08-02 11:21:12 +0700

    Clean data in csv transaction

--- airflow/dags/product_price_pipeline.py
+++ airflow/dags/product_price_pipeline.py
@@ -14,21 +14,32 @@ dag = DAG('product_price_pipeline',
             catchup=False)
 
 def get_product_upc_and_description():
-    df = pandas.read_csv('products-lookup-table.csv', header=1)
+    df = pandas.read_csv('dataset/products-lookup-table.csv', header=1)
     new_df = df[['UPC', 'DESCRIPTION']]
-    new_df.to_csv('product_upc_and_description.csv', index=False)
+    new_df.to_csv('dataset/product_upc_and_description.csv', index=False)
 
 def get_transaction_upc_and_price():
-    df = pandas.read_csv('transaction-table.csv', header=1)
+    df = pandas.read_csv('dataset/cleaned_transaction_data.csv')
     new_df = df[['UPC', 'PRICE']]
-    new_df.to_csv('transaction_upc_and_price.csv', index=False)
+    new_df.to_csv('dataset/transaction_upc_and_price.csv', index=False)
 
 def merge():
-    df_product_description = pandas.read_csv('product_upc_and_description.csv')
-    df_product_price = pandas.read_csv('transaction_upc_and_price.csv')
+    df_product_description = pandas.read_csv('dataset/product_upc_and_description.csv')
+    df_product_price = pandas.read_csv('dataset/transaction_upc_and_price.csv')
     new_df = pandas.merge(df_product_description, df_product_price, left_on='UPC', right_on='UPC')
     # new_df = df_product_description.join(df_product_price, on='UPC', how='left')
-    new_df.to_csv('product_and_price.csv', index=False)
+    new_df.to_csv('dataset/product_and_price.csv', index=False)
+
+def remove_outliners():
+    df = pandas.read_csv('dataset/transaction-table.csv', header=1)
+    valid_data = df['UNITS'] >= df['VISITS']
+    new_df = df[valid_data]
+    new_df.to_csv('dataset/cleaned_transaction_data.csv', index=False)
+
+def include_vat():
+    df = pandas.read_csv('dataset/product_and_price.csv')
+    df['PRICE_WITH_VAT'] = df['PRICE'] * 1.08
+    df.to_csv('dataset/product_and_price_with_vat.csv', index=False)
 
 start = DummyOperator(
                 task_id='start', 
@@ -38,6 +49,12 @@ start = DummyOperator(
 get_product_upc_and_description = PythonOperator(
                 task_id='get_product_upc_and_description',
                 python_callable=get_product_upc_and_description,
+                dag=dag
+)
+
+remove_outliners = PythonOperator(
+                task_id='remove_outliners',
+                python_callable=remove_outliners,
                 dag=dag
 )
 
@@ -53,9 +70,17 @@ merge = PythonOperator(
                 dag=dag
 )
 
+include_vat = PythonOperator(
+                task_id='include_vat',
+                python_callable=include_vat,
+                dag=dag
+)
+
 end = DummyOperator(task_id='end', 
                 dag=dag
 )
 
-
-start >> [get_product_upc_and_description, get_transaction_upc_and_price] >> merge >> end
+start >> [get_product_upc_and_description, remove_outliners]
+get_product_upc_and_description >> merge
+remove_outliners >> get_transaction_upc_and_price >> merge
+merge >> include_vat >> end
